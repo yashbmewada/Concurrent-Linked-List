7c374095c47757b01b7b60f249270087492d947a
garima1313 <garima1357@gmail.com>
2018-11-26 19:17:37 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,67 @@
 import java.util.Random;
 
+
+
+
+
 public class Main {
-	static int BOUND = 50;
-	static int NUMBER = 8;
-	public static void main(String[] args) throws InterruptedException {
-		
-
-		LazyLinkedList<Integer> list = new LazyLinkedList<>();
+	static final int THREAD = 8;
+	LazyLinkedList<Integer> list = new LazyLinkedList<>();
+	static int x=0;
+	
+	
+	public void test() throws Exception {
 		System.out.println("list created : " + list.toString());
-		Random r = new Random();
-
-
 		System.out.printf(list.toString());
-		Thread[] threads = new Thread[NUMBER];
-		for (int i = 0; i < NUMBER; ++i) {
-			threads[i] = new Thread(new Runnable() {
-				@Override
-				public void run() {
-					for (int i = 0; i < 1; ++i) {
-						Integer integer = r.nextInt(BOUND);
-						list.add(integer);
-						try {
-							Thread.sleep(500);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-						if (list.contains(integer)) {
-							// list.remove(integer);
-							System.out.println("EXISTS");
-						}
-
-					}
-				}
-			});
+		Thread[] threads = new Thread[THREAD];
+		for (int i = 0; i < THREAD; ++i) {
+			threads[i]= new MyThread();
+		}
+		for (int i = 0; i < THREAD; ++i) {
+			System.out.println("thread"+ i);
 			threads[i].start();
 		}
-		for(int i = 0; i < NUMBER; ++i) {
+		for(int i = 0; i < THREAD; ++i) {
 			threads[i].join();
 
 		}
 		System.out.println(list.toString());
 		
+		
+	}
+	public static void main(String[] args) throws InterruptedException {
+		Main mpt = new Main(); // Would like to be able to pass this # of threads
 
+		try {
+			mpt.test();
+		} 
+		catch (Exception e) {
+		}
+
+	}
+	class MyThread extends Thread {
+		Random r = new Random();
+		public void run() {
+			for (int i = 0; i < 1; ++i) {
+				//Integer integer = r.nextInt(50);
+				Integer integer=x++;
+				list.add(integer);
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				if (list.contains(integer)) {
+					System.out.println(integer+ "EXISTS"+ (integer -1));
+					System.out.printf(list.toString());
+					list.remove(integer+1);
+					//System.out.println("---"+list.toString());
+					list.replace(integer, integer-1);
+					//System.out.println("---"+list.toString());
+					
+				}
+
+			}
+		}
 	}
 }
